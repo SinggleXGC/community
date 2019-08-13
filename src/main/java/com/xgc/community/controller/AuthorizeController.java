07eac84919f06dc5bb71059a334795d01141f958
SinggleXGC <1307708161@qq.com>
2019-08-13 11:42:00 +0800

    将获取到的user信息放到session里

--- src/main/java/com/xgc/community/controller/AuthorizeController.java
+++ src/main/java/com/xgc/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -29,7 +31,7 @@ public class AuthorizeController {
      * GitHub接收到请求后，会回调 /callback ,返回一个Code
      */
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -38,9 +40,13 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getBio());
-        System.out.println(user.getId());
-        return "index";
+        if (user != null){
+            //登录成功，将user写入session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
