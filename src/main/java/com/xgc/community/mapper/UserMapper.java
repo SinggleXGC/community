49ede188b6f27a5e5570d075fc8ee98adfefe6ef
SinggleXGC <1307708161@qq.com>
2019-08-15 23:03:55 +0800

    实现持久化登录状态获取，即在进入登录页面时对cookie的token进行验证，验证用户登录状态

--- src/main/java/com/xgc/community/mapper/UserMapper.java
+++ src/main/java/com/xgc/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.xgc.community.mapper;
 import com.xgc.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -10,4 +12,6 @@ public interface UserMapper {
     @Insert("insert into \"user\" (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from \"user\" where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
