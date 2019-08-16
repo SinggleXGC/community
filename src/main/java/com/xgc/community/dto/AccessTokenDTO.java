86e4a87f65fbd078d58fef58912e02eedb95a18a
SinggleXGC <1307708161@qq.com>
2019-08-16 23:30:59 +0800

    添加Lombok支持，同时添加获取头像地址的字段。

--- src/main/java/com/xgc/community/dto/AccessTokenDTO.java
+++ src/main/java/com/xgc/community/dto/AccessTokenDTO.java
@@ -1,9 +1,12 @@
 package com.xgc.community.dto;
 
+import lombok.Data;
+
 /**
  * DTO 就是数据传输对象
  * 封装了获取AccessToken需要传递的参数
  */
+@Data
 public class AccessTokenDTO {
 
     private String client_id;
@@ -11,45 +14,5 @@ public class AccessTokenDTO {
     private String code;
     private String redirect_uri;
     private String state;
-
-    public String getClient_id() {
-        return client_id;
-    }
-
-    public void setClient_id(String client_id) {
-        this.client_id = client_id;
-    }
-
-    public String getClient_secret() {
-        return client_secret;
-    }
-
-    public void setClient_secret(String client_secret) {
-        this.client_secret = client_secret;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getRedirect_uri() {
-        return redirect_uri;
-    }
-
-    public void setRedirect_uri(String redirect_uri) {
-        this.redirect_uri = redirect_uri;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 
 }
