09381ef76726108911eb1578b24606ad3bb81a0e
SinggleXGC <1307708161@qq.com>
2019-08-25 14:23:19 +0800

    添加拦截器实现验证用户登录的功能

--- src/main/java/com/xgc/community/controller/IndexController.java
+++ src/main/java/com/xgc/community/controller/IndexController.java
@@ -1,10 +1,6 @@
 package com.xgc.community.controller;
 
 import com.xgc.community.dto.PaginationDTO;
-import com.xgc.community.dto.QuestionDTO;
-import com.xgc.community.mapper.QuestionMapper;
-import com.xgc.community.mapper.UserMapper;
-import com.xgc.community.model.User;
 import com.xgc.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,15 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
@@ -30,20 +21,7 @@ public class IndexController {
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
                         @RequestParam(name = "size",defaultValue = "5") Integer size
                         ){
-        Cookie[] cookies = request.getCookies();
 
-        if (cookies != null) {
-            for (Cookie cookie : cookies){
-                if (cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
-            }
-        }
 
         PaginationDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination",pagination);
